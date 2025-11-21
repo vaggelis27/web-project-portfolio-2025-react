0f857a3ccafd91fc15501686172e5783edc012fd
vaggelis27 <e.ntosikas@gmail.com>
2025-11-21 02:39:22 +0200

    update 21/10

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,6 @@
 import Navbar from "../layout/Navbar/index.js";
-import "../layout/Navbar/Navbar.css";
 import Footer from "../layout/Footer/index.js";
-import "../layout/Footer/Footer.css";
-import "../../main.jsx";
-import "../../index.css";
 import HeroCarousel from "../sections/HeroCarousel/index.js";
-import "../sections/HeroCarousel/HeroCarousel.css";
 import About from "../sections/About/index.js";
 import Contact from "../sections/Contact/index.js";
 import Portfolio from "../sections/Portfolio/index.js";
--- src/components/sections/Portfolio/index.js
+++
@@ -1,2 +0,0 @@
-export { default } from "./Portfolio.jsx";
-
