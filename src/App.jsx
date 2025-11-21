2c7f15970c77b3a702847ace6f70e9cfe2cd2cc8
vaggelis27 <e.ntosikas@gmail.com>
2025-11-21 13:48:19 +0200

    update 21/10

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import HeroCarousel from "./components/HeroCarousel/HeroCarousel.jsx";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <HeroCarousel />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="text-center text-light py-4 bg-dark footer-section ">
+      <div className="white mb-2 footer-title">
+        <strong>Photography Portfolio</strong>
+      </div>
+
+      <div className="my-2 footer-social">
+        <a
+          href="https://www.instagram.com/vagelisntotsikas/"
+          target="_blank"
+          title="Instagram Profile"
+          className="mx-2 footer-social-link footer-social-link--instagram"
+        >
+          <i className="bi bi-instagram"></i>
+        </a>
+
+        <a
+          href="https://github.com/vaggelis27"
+          target="_blank"
+          title="GitHub Profile"
+          className="mx-2 footer-social-link footer-social-link--github"
+        >
+          <i className="bi bi-github"></i>
+        </a>
+
+        <a
+          href="https://www.facebook.com/vagelis.ntotsikas/"
+          target="_blank"
+          title="View Facebook Profile"
+          className="mx-2 footer-social-link footer-social-link--facebook"
+        >
+          <i className="bi bi-facebook"></i>
+        </a>
+      </div>
+
+      <div className="small text-white">
+        &copy; 2025 Evangelos Ntotsikas. All Rights Reserved.
+      </div>
+    </footer>
+  );
+}
