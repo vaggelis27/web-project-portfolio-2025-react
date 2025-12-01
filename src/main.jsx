c72a53d6d529eb12a2962e2f4fe0506305308c76
vaggelis27 <e.ntosikas@gmail.com>
2025-12-01 16:05:35 +0200

    update 01/12

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
 
 import "./index.css";
@@ -12,8 +12,8 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
