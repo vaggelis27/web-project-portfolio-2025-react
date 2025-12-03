53e37edb4b3e88e627a2374ff304aee83a2b0ee4
vaggelis27 <e.ntosikas@gmail.com>
2025-12-03 02:01:32 +0200

    update 03/12

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
-import logo from "../assets/new logo 2025 2.png";
+import logo from "../assets/brand-logo.svg";
 
 export default function Navbar() {
   const canvasRef = useRef(null);
