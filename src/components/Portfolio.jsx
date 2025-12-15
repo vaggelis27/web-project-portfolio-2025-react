4c9e5076f153a876df8264d1d754b293ee4c7ebe
vaggelis27 <e.ntosikas@gmail.com>
2025-12-15 14:15:20 +0200

    update 15/12

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -15,60 +15,52 @@ const portfolioItems = [
     id: 1,
     title: "Nature",
     image: portfolioImage1,
-    to: "/naturePage",
+    to: "/nature",
   },
   {
     id: 2,
     title: "Portraiture",
     image: portfolioImage2,
-    to: "/portraitPage",
+    to: "/portrait",
   },
   {
     id: 3,
     title: "Nightscapes",
     image: portfolioImage3,
-    to: "/nightPage",
+    to: "/night",
   },
 ];
 
 export default function Portfolio() {
   return (
     <section id="portfolio" className="py-5 my-0 text-dark">
-      <div>
-        <div classname="wave"></div>
-        <div classname="wave"></div>
-        <div classname="wave"></div>
-
-        <div className="background-logo">
-          <div className="Portfolio">
-            <Container className=" text-center">
-              <h1 className="fw-bold mb-4">Portfolio</h1>
-              <Row className="g-4">
-                {portfolioItems.map((item) => (
-                  <Col md={4} key={item.id}>
-                    {/* link cards */}
-                    <Link to={item.to} className="Portfolio-card-link">
-                      <div className="Portfolio-card">
-                        {/* img full frame */}
-                        <img
-                          src={item.image}
-                          className="Portfolio-img"
-                          alt={item.title}
-                        />
-                        {/* Το Overlay up images*/}
-                        <div className="Portfolio-overlay">
-                          <h3 className="Overlay-title">{item.title}</h3>
-                          <span className="Overlay-tag">
-                            View Project &rarr;
-                          </span>
-                        </div>
+      <div className="background-logo">
+        <div className="Portfolio">
+          <Container className=" text-center">
+            <h1 className="fw-bold mb-4">Portfolio</h1>
+            <Row className="g-4">
+              {portfolioItems.map((item) => (
+                <Col md={4} key={item.id}>
+                  {/* link cards */}
+                  <Link to={item.to} className="Portfolio-card-link">
+                    <div className="Portfolio-card">
+                      {/* img full frame */}
+                      <img
+                        src={item.image}
+                        className="Portfolio-img"
+                        alt={item.title}
+                      />
+                      {/* Το Overlay up images*/}
+                      <div className="Portfolio-overlay">
+                        <h3 className="Overlay-title">{item.title}</h3>
+                        <span className="Overlay-tag">View Project &rarr;</span>
                       </div>
-                    </Link>
-                  </Col>
-                ))}
-              </Row>
-            </Container>
-          </div>
+                    </div>
+                  </Link>
+                </Col>
+              ))}
+            </Row>
+          </Container>
         </div>
       </div>
     </section>
